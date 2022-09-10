a3ce6734b843521fde27900ac64d96c2ca0d74af
zinzin30 <zinzin30@gmail.com>
2022-09-10 21:23:51 +0700

    Step 4 - Check Which Button is Pressed

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
-alert("hello");
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+});
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100);
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
