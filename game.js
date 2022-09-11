4086471cd9b29fb1242a1213b083b231bc454b95
zinzin30 <zinzin30@gmail.com>
2022-09-11 13:46:30 +0700

    Step 7 - Start the Game

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").click(function () {
   var userChosenColor = $(this).attr("id");
@@ -10,12 +20,18 @@ $(".btn").click(function () {
 });
 
 function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
   $("#" + randomChosenColor)
     .fadeIn(100)
-    .fadeOut(100);
+    .fadeOut(100)
+    .fadeIn(100);
+
   playSound(randomChosenColor);
 }
 
