765ac96ac52c329f5cb0ae04211868a01051be8a
zinzin30 <zinzin30@gmail.com>
2022-09-12 13:11:26 +0700

    Step 8 - Check the User's Answer Against the Game Sequence

--- game.js
+++ game.js
@@ -14,18 +14,52 @@ $(document).keypress(function () {
 
 $(".btn").click(function () {
   var userChosenColor = $(this).attr("id");
+
   userClickedPattern.push(userChosenColor);
+  console.log("userClickedPattern: ", userClickedPattern);
+  console.log("gamePattern: ", gamePattern);
+
   playSound(userChosenColor);
   animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  console.log("currentLevel: ", currentLevel);
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log(
+      "gamePattern[currentLevel] === userClickedPattern[currentLevel]: ",
+      gamePattern[currentLevel] === userClickedPattern[currentLevel]
+    );
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+
+    setTimeout(function () {
+      document.location.reload();
+    }, 1000);
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+  console.log("userClickedPattern: ", userClickedPattern);
+
   level++;
   $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
+
   gamePattern.push(randomChosenColor);
+  console.log("gamePattern: ", gamePattern);
 
   $("#" + randomChosenColor)
     .fadeIn(100)
